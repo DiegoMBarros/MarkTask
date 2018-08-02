d44d2502ef116740efb348e2187990b4221d24ee
DiegoMBarros <39345818+DiegoMBarros@users.noreply.github.com>
2018-08-02 00:50:43 -0300

    Aula 5 02-08-18

--- features/support/helpers.rb
+++ features/support/helpers.rb
@@ -1,5 +1,5 @@
 class Helpers
-    def login(email,senha)
+    def login(email, senha)
         result = HTTParty.post(
             'https://marktasks.herokuapp.com/api/login',
             headers: { 'Content-Type' => 'application/json' },
@@ -21,18 +21,19 @@ class Helpers
         )
 
         @tasks = result.parsed_response['data']
+        # puts @tasks = result.parsed_response['data'].class #-> Para descobrir o titulo.
 
         @tasks.each do |t|
             @id_para_remover = t['_id'] if t['title'] == titulo
         end
-
+            # puts @id_para_remover
         # @tarefa = @tasks.select { |t| t['title'] == titulo }.first #-> outra maneira de selecionar o item da lista
-
+        
         HTTParty.delete(
             'https://marktasks.herokuapp.com/api/tasks/' + @id_para_remover, #-> Se optar pela opção comentada, então o id deve ser declarado assim "@tarefa['_id']"
             headers: @headers
         )
-        end
+    end
     
 
     def back_to_the_past(email)
